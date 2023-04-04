f7cf38c0ef52441ff5ea9e6b59a6572bba7c3f4b
brandonjoubran <brandonjoubran@gmail.com>
2023-04-04 16:27:30 -0400

    Can play Spotify playlist and refresh access token

--- index.js
+++ index.js
@@ -65,6 +65,68 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+function getNewSpotifyAccessToken() {
+	const authString = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
+
+	// Make a request to the Spotify API to get a new access token
+	return fetch('https://accounts.spotify.com/api/token', {
+		method: 'POST',
+		headers: {
+		'Authorization': `Basic ${authString}`,
+		'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: 'grant_type=client_credentials'
+	})
+	.then(response => response.json())
+	.then(data => {
+		const accessToken = data.access_token;
+		process.env.SPOTIFY_ACCESS_TOKEN = accessToken
+		return true
+	// Use the new access token to make requests to the Spotify API
+	})
+	.catch(error => {
+		console.error(error);
+	});
+
+}
+
+function getSpotifyTracks(playlistId) {
+	// Make a request to the Spotify API
+	console.log("2")
+	return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+		headers: {
+		'Authorization': `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
+		}
+	})
+	.then(response => {
+		console.log("3")
+		return response.json()}
+		)
+	.then(data => {
+		console.log("4")
+		return data
+	})
+}
+
+function getSpotifyTracksRetry(playlistId) {
+	console.log("1")
+	return getSpotifyTracks(playlistId)
+	.then(data => {
+		if (data.error.status == 401) {
+			console.log("5")
+			return getNewSpotifyAccessToken()
+			.then(status => {
+				console.log("6")
+				return getSpotifyTracks(playlistId)
+			})
+		}
+		return data
+	})
+}
+
+function addToQueue(item) {
+	client.queue.push(item)
+}
 
 function getYoutubeResource(url) {
 	const stream = ytdl(url, {
@@ -79,10 +141,35 @@ function getYoutubeResource(url) {
 	return resource
 }
 
+// Function to get YouTube link for a given request
+function searchYoutube(query, message) {
+
+	var opts = {
+		maxResults: 10,
+		key: process.env.GOOGLE_KEY
+	  };
+	  
+	  // Searching YouTube
+	  search(query, opts)
+	  .then(results => {
+		let link = results.results[0].link
+		curSong = results.results[0].title;
+		message.channel.send(`Now playing: ${curSong}`);
+		return getYoutubeResource(link)
+	})
+	.then(resource => {
+		playSong(resource, message)
+	})	
+	.catch( err => console.log(err));
+
+}
+
+// Function to play song given the resource needed
 function playSong(resource, message) {
 
 	const player = client.player
 
+	// Bot join voice channel
 	const connection = joinVoiceChannel({
 		channelId: message.member.voice.channel.id,
 		guildId: message.member.voice.channel.guild.id,
@@ -91,14 +178,12 @@ function playSong(resource, message) {
 	console.log("joined voice channel")
 	console.log("is song currently played: " + player.state.status != 'playing')
 
-	console.log('1')
+	// Play song
 	player.play(resource);
-	console.log('2')
 	connection.subscribe(player);
-	console.log('3')
 }
 
-function parseRequest(query, message) {
+function handleRequest(query, message) {
 	if (query.startsWith('https://www.youtube.com/')) {
 			// Playing a YouTube song
 			resource = getYoutubeResource(query)
@@ -112,71 +197,100 @@ function parseRequest(query, message) {
 			// https://open.spotify.com/playlist/1unCkH5i66vPZUI2ZA8R8H?si=5f29bd6644524455
 			// Playing a Spotify playlist
 
+			// Parsing link to get playlist id
+			let playlistId = query.split('/').pop().split('?')[0];
+
+		 	getSpotifyTracksRetry(playlistId)
+			.then(data => {
+				console.log(data)
+
+				// Play the first song in the playlist
+				searchYoutube(data.items[0].track.name + ' ' + data.items[0].track.artists[0].name, message)
+
+				// Add the rest of the playlist to the queue
+				for (let i = 1; i < data.items.length; i++) {
+					console.log("Adding to queue from playlist: " + data.items[i].track.name + ' ' + data.items[i].track.artists[0].name)
+					addToQueue(data.items[i].track.name + ' ' + data.items[i].track.artists[0].name)
+				}
+				console.log("current queue: [" + client.queue + "]") 
+			})
+
+			// Make a request to the Spotify API
+			/*fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+				headers: {
+				'Authorization': `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
+				}
+			})
+			.then(response => {
+				if (response.status === 401) {
+					return getNewSpotifyAccessToken()
+					.then(() => {
+						
+					})
+					for (let i = 0; i < 3; i++) {
+
+					}
+
+
+				  // Access token has expired
+				  console.log('Access token expired. Refreshing...');
+				  return refreshAccessToken()
+				  .then(newToken => fetchPlaylistTracks(playlistId)) 
+				} else {
+				return response.json();
+			  }})
+			.then(data => {
+				console.log(data)
+
+				// Play the first song in the playlist
+				searchYoutube(data.items[0].track.name + ' ' + data.items[0].track.artists[0].name, message)
+
+				// Add the rest of the playlist to the queue
+				for (let i = 1; i < data.items.length; i++) {
+					console.log("Adding to queue from playlist: " + data.items[i].track.name + ' ' + data.items[i].track.artists[0].name)
+					addToQueue(data.items[i].track.name + ' ' + data.items[i].track.artists[0].name)
+				}
+				console.log("current queue: [" + client.queue + "]") 
+
+			})
+			.catch(error => {
+				console.error(error);
+			});*/
+
+
 		} else {
 			// Have to search for song
-			var opts = {
-				maxResults: 10,
-				key: 'AIzaSyANwSNsY6gaZ-1S8XO5ozEgdaR9a-6xDaI'
-			  };
-			  
-			  search(query, opts)
-			  .then(results => {
-				console.log(results)
-				let link = results.results[0].link
-				curSong = results.results[0].title;
-				message.channel.send(`Now playing: ${curSong}`);
-				return getYoutubeResource(link)
-			})
-			.then(resource => {
-				playSong(resource, message)
-			})	
-			.catch( err => console.log(err));
+			searchYoutube(query, message)
+
 		}
 
 }
 
+/*curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=754d782c52814d73b97966fd328e803f&client_secret=e89722e302f54782b59669b2ae7f6309"
+*/
+
 client.on(Events.MessageCreate, (message) => {
 
 	if (message.content.startsWith('!skip')) {
+		// Stopping player makes it idle which triggers next song
 		console.log("Trying to stop...")
 		client.player.stop()
-
-		console.log(client.queue.length === 0)
-		if (client.queue != 0) {
-			console.log(client.queue)
-			console.log(client.queue.length)
-			console.log("idle!")
-			let next = client.queue.pop()
-			console.log(client.queue)
-			console.log(next)
-			const stream = ytdl(next, {
-				filter: "audioonly",
-				});
-			console.log("here?1")
-			const resource = createAudioResource(stream, {
-				inputType: StreamType.Arbitrary,
-				});
-			console.log("here?2")
-			client.player.play(resource);
-			console.log("here?3")
-			message.channel.send(`Now playing: `);
-			console.log("here?4")
-		}
 	}
 
-	if (message.content.startsWith('!stop')) {
+	if (message.content.startsWith('!pause')) {
 		console.log("Trying to stop...")
-		client.player.stop()
+		client.player.pause()
 	}
 
 	if (message.content.startsWith('!q')) {
 		const query = message.content.slice(3);
-		client.queue.push(query)
+		addToQueue(query)
 		message.channel.send(`Added to queue`);
-		console.log("current queue: [" + client.queue + "]") 
 	}
 
-	if (message.content.startsWith('!play')) {
+	if (message.content.startsWith('!play') || message.content.startsWith('!p')) {
 
 		// Slicing song from command
 		const query = message.content.slice(6);
@@ -185,11 +299,11 @@ client.on(Events.MessageCreate, (message) => {
 		// Getting voice channel
 	  	const voiceChannel = message.member.voice.channel;
 
-	
+		// Ensuring user is in the voice channel
 	  	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
 	  	console.log("verified user is in the voice channel")	
 
-		parseRequest(query, message)
+		handleRequest(query, message)
 
 	  	try {
 
@@ -199,10 +313,11 @@ client.on(Events.MessageCreate, (message) => {
 			});*/
 
 			client.player.on(AudioPlayerStatus.Idle, () => {
-				console.log("2. current queue: [" + client.queue + "]") 
+				// Playing next song in queue if queue not empty
 				if (client.queue.length > 0) { 
-					let next = client.queue.pop()
-					parseRequest(next, message)
+					let next = client.queue.shift()
+					console.log('next is ' + next)
+					handleRequest(next, message)
 				}
 				//connection.subscribe(player);
 				//message.channel.send(`Now playing next song`);
