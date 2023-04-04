f4d5ff859d433b3576690400124109d652197a55
brandonjoubran <brandonjoubran@gmail.com>
2023-04-04 12:57:44 -0400

    Cleaned up code, play and queue working. Weird try without catch error still being investigated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ytdl = require('ytdl-core');
+const search = require("youtube-search")
 const fs = require('node:fs');
 const path = require('node:path');
 require("dotenv").config(); //to start process from .env file
@@ -54,12 +55,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 })();
 
 // Creating the player
-client.player = new Player(client, {
-	ytdlOptions: {
-		quality: "highestaudio", 
-		highWaterMark: 1 << 25,
-	}
-})
+client.player = createAudioPlayer();
+
+client.queue = []
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -68,72 +66,148 @@ client.once(Events.ClientReady, c => {
 });
 
 
+function getYoutubeResource(url) {
+	const stream = ytdl(url, {
+		filter: "audioonly",
+		quality: 'highestaudio',
+      	highWaterMark: 1<<25,
+	})
+
+	const resource = createAudioResource(stream, {
+		inputType: StreamType.Arbitrary,
+	})
+	return resource
+}
+
+function playSong(resource, message) {
+
+	const player = client.player
+
+	const connection = joinVoiceChannel({
+		channelId: message.member.voice.channel.id,
+		guildId: message.member.voice.channel.guild.id,
+		adapterCreator: message.member.voice.channel.guild.voiceAdapterCreator,
+	});
+	console.log("joined voice channel")
+	console.log("is song currently played: " + player.state.status != 'playing')
+
+	console.log('1')
+	player.play(resource);
+	console.log('2')
+	connection.subscribe(player);
+	console.log('3')
+}
+
+function parseRequest(query, message) {
+	if (query.startsWith('https://www.youtube.com/')) {
+			// Playing a YouTube song
+			resource = getYoutubeResource(query)
+			playSong(resource, message)
+		
+		} else if (query.startsWith('https://open.spotify.com/track/')) {
+			// https://open.spotify.com/track/2mN6HgN5Cm4slbh35jiDOa?si=1a33bb929eb04840
+			// Playing a song from Spotify
+
+		} else if (query.startsWith('https://open.spotify.com/playlist/')) {
+			// https://open.spotify.com/playlist/1unCkH5i66vPZUI2ZA8R8H?si=5f29bd6644524455
+			// Playing a Spotify playlist
+
+		} else {
+			// Have to search for song
+			var opts = {
+				maxResults: 10,
+				key: 'AIzaSyANwSNsY6gaZ-1S8XO5ozEgdaR9a-6xDaI'
+			  };
+			  
+			  search(query, opts)
+			  .then(results => {
+				console.log(results)
+				let link = results.results[0].link
+				curSong = results.results[0].title;
+				message.channel.send(`Now playing: ${curSong}`);
+				return getYoutubeResource(link)
+			})
+			.then(resource => {
+				playSong(resource, message)
+			})	
+			.catch( err => console.log(err));
+		}
+
+}
+
 client.on(Events.MessageCreate, (message) => {
+
+	if (message.content.startsWith('!skip')) {
+		console.log("Trying to stop...")
+		client.player.stop()
+
+		console.log(client.queue.length === 0)
+		if (client.queue != 0) {
+			console.log(client.queue)
+			console.log(client.queue.length)
+			console.log("idle!")
+			let next = client.queue.pop()
+			console.log(client.queue)
+			console.log(next)
+			const stream = ytdl(next, {
+				filter: "audioonly",
+				});
+			console.log("here?1")
+			const resource = createAudioResource(stream, {
+				inputType: StreamType.Arbitrary,
+				});
+			console.log("here?2")
+			client.player.play(resource);
+			console.log("here?3")
+			message.channel.send(`Now playing: `);
+			console.log("here?4")
+		}
+	}
+
+	if (message.content.startsWith('!stop')) {
+		console.log("Trying to stop...")
+		client.player.stop()
+	}
+
+	if (message.content.startsWith('!q')) {
+		const query = message.content.slice(3);
+		client.queue.push(query)
+		message.channel.send(`Added to queue`);
+		console.log("current queue: [" + client.queue + "]") 
+	}
+
 	if (message.content.startsWith('!play')) {
+
+		// Slicing song from command
 		const query = message.content.slice(6);
-		console.log("here2")
-	  const voiceChannel = message.member.voice.channel;
-	  if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
-	  console.log("here3")
+		console.log("sliced query: " + query)
 
-	  /*const queue = client.player.createQueue(message.guild, {
-		metadata: {
-		  channel: message.channel
-		}
-	  });*/
-	  
-	  console.log("here4")
-	  console.log(query)
-	  try {
-		const player = createAudioPlayer();
+		// Getting voice channel
+	  	const voiceChannel = message.member.voice.channel;
 
-		const connection = joinVoiceChannel({
-			channelId: message.member.voice.channel.id,
-			guildId: message.member.voice.channel.guild.id,
-			adapterCreator: message.member.voice.channel.guild.voiceAdapterCreator,
+	
+	  	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+	  	console.log("verified user is in the voice channel")	
+
+		parseRequest(query, message)
+
+	  	try {
+
+			/*client.player.on('error', error => {
+				console.error(`Error: ${error}`);
+				//client.player.play(getNextResource());
+			});*/
+
+			client.player.on(AudioPlayerStatus.Idle, () => {
+				console.log("2. current queue: [" + client.queue + "]") 
+				if (client.queue.length > 0) { 
+					let next = client.queue.pop()
+					parseRequest(next, message)
+				}
+				//connection.subscribe(player);
+				//message.channel.send(`Now playing next song`);
+
 		});
-		/*const stream = ytdl(query, {
-			filter: "audioonly",
-		  });
-		const resource = createAudioResource(stream, {
-			inputType: StreamType.Arbitrary,
-		 });		//await queue.play('https://www.myinstants.com/media/sounds/baka.mp3');
-		*/
-		 console.log(player.state)
-		 if (player.state.status == 'playing') {
-			console.log("SONG ALREADY PLAYING")
-
-		 }
-		 player.on(AudioPlayerStatus.Idle, () => {
-			console.log("idle")
-			const stream = ytdl("https://www.youtube.com/watch?v=JuYeHPFR3f0", {
-				filter: "audioonly",
-			  });
-			const resource = createAudioResource(stream, {
-				inputType: StreamType.Arbitrary,
-			 });
-			player.play(resource);
-			message.channel.send(`Now playing: `);
-		});
-
-		player.on(AudioPlayerStatus.Playing, () => {
-			console.log("SONG ALREADY PLAYING 2")
-			console.log("Add to queue...")
-			return
-		});
-		console.log(AudioPlayerStatus.Playing)
-		console.log(player.state)
-		if (true) {
-			const stream = ytdl(query, {
-				filter: "audioonly",
-				});
-			const resource = createAudioResource(stream, {
-				inputType: StreamType.Arbitrary,
-				});
-			player.play(resource);
-			message.channel.send(`Now playing: `);
-			connection.subscribe(player);
-		}
 
 	  } catch (error) {
 		console.error(error);
