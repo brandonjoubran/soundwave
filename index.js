7c89424f58fb42fc91dcab4fee546dc81244be48
brandonjoubran <brandonjoubran@gmail.com>
2025-01-27 23:18:41 -0500

    Migrated successfully, have to download video now. Also added qn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ytdl = require('ytdl-core');
+const ytdl = require("@distube/ytdl-core");
 const search = require("youtube-search")
 const fs = require('node:fs');
 const path = require('node:path');
@@ -7,7 +7,6 @@ require("dotenv").config(); //to start process from .env file
 const { joinVoiceChannel } = require('@discordjs/voice');
 const { createAudioPlayer, NoSubscriberBehavior, createAudioResource, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
 const fetch = require('node-fetch');
-
 
 const { Client, Events, Collection, GatewayIntentBits } = require('discord.js')
 
@@ -69,6 +68,7 @@ let prevMsg = ''
 
 
 function htmlDecode (input) {
+	// https://www.toptal.com/designers/htmlarrows/punctuation/figure-dash/
 	return input.replace('&#38;', "&")
 				.replace('&#60;', "<")
 				.replace('&#62;', ">")
@@ -93,6 +93,14 @@ try {
 	client.player.on('error', error => {
 		console.error(`Error: ${error}`);
 		client.player.play(getNextResource());
+		// console.log("Queue is: " + client.queue)
+		// const next = client.queue.shift(); // Get the next song from the queue
+		// if (next) {
+		// console.log("Next is: " + next)
+		// handleRequest(next); // Play the next song
+		// } else {
+		// console.log('Queue is empty');
+		// }
 	});
 
 	client.player.on(AudioPlayerStatus.Paused, () => {
@@ -209,20 +217,50 @@ function addToQueue(item) {
 	client.queue.push(item)
 }
 
-// Downloading song audio from YouTube
-function getYoutubeResource(url) {
-	const stream = ytdl(url, {
-		quality: 'highestaudio',
-      	highWaterMark: 1<<25,
-	})
+function addToBegginingOfQueue(item) {
+	console.log(client.queue)
+	client.queue.unshift(item)
+	console.log(client.queue)
+}
 
-	const resource = createAudioResource(stream, {
-		inputType: StreamType.Arbitrary,
-		inlineVolume: true,
-	})
+// Downloading song audio from YouTube
+async function getYoutubeResource(url) {
+	console.log("pls1 " + url)
+	// stream = ytdl(url, {
+	// 	filter: 'audio',
+	//  })
+	 //ytdl(url).pipe(require("fs").createWriteStream("video.mp4"));
+	 await new Promise((resolve) => { // wait
+		ytdl(url, {filter: 'audioonly'}).pipe(require("fs").createWriteStream("video.mp4"))
+		.on('close', () => {
+		  resolve(); // finish
+		})
+	  })
+
+	// Get video info
+// ytdl.getBasicInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
+// 	console.log(info.videoDetails.title);
+//   });
+  
+  // Get video info with download formats
+  //info = await ytdl.getInfo(url)
+  console.log("pls2 ")
+  //console.log(info.formats)
+  //const audioItem = info.formats.find(info => info.mimeType === 'audio/mp4; codecs="mp4a.40.2"');
+// console.log("audioItem " + audioItem.url)
+
+//   console.log("items " + audioItem)
+  test = 'https://rr2---sn-gvbxgn-tt1e7.googlevideo.com/videoplayback?expire=1736140280&ei=mBF7Z_q6ErjBlu8Pzu2M6Qo&ip=99.246.174.72&id=o-AATJGE5EFd6OEclWa2a1Pw5fE_saWyJHyIy4h59D4j_v&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1736118680%2C&mh=m2&mm=31%2C26&mn=sn-gvbxgn-tt1e7%2Csn-vgqsknse&ms=au%2Conr&mv=m&mvi=2&pcm2cms=yes&pl=18&rms=au%2Cau&gcr=ca&initcwndbps=4633750&bui=AfMhrI97d5Em0cOdyLKC3oAc9olAwJJ085XK4ZcK9NCLmWVxiKBfE010-qAFQHLNLmBub3cWZWkOuhTM&spc=x-caUBL6xgqaMFOYmN48hIdSPwlU53dZ1arL-5ZER6RGm8O7AHD10uqcEb8C&vprv=1&svpuc=1&mime=audio%2Fmp4&rqh=1&gir=yes&clen=3247302&dur=200.597&lmt=1728734922741583&mt=1736118291&fvip=2&keepalive=yes&fexp=51326932%2C51335594%2C51371294&c=IOS&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAOG3xXcAvI5tsqUb2Ntk8QvkRmay3UFwmMCma_uGV3ZzAiBf2wJVLSyjsiUedJWjLIp6Iae9nTI6NGdeGeUu42INsQ%3D%3D&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Crms%2Cinitcwndbps&lsig=AGluJ3MwRQIgUDr3yq28BlaNSvRK0LAU7ZHDfW6nC_n1eQSfr2Fv7J4CIQC9AgML_8Myj1pbNXCJg7t68FXnxXAVdjXMaj3dzD_Yng%3D%3D'
+  //const audioItem = items.find(item => item.mimeType === 'audio/mp4; codecs="mp4a.40.2"');
+
+	// console.log("stream " + audioItem)
+	// console.log("pls2 " + audioItem)
+
+	const resource = createAudioResource("./video.mp4")
+	console.log("pls3")
 	console.log("resource", resource)
-	console.log("resource.volume", resource.volume)
-	resource.volume.setVolume(0.75);
+	// console.log("resource.volume", resource.volume)
+	// resource.volume.setVolume(0.75);
 	return resource
 }
 
@@ -237,10 +275,16 @@ function searchYoutube(query) {
 	  // Searching YouTube
 	  search(query, opts)
 	  .then(results => {
+		console.log(results)
+		if (results.status === 403) {
+			console.error('Forbidden: Check your credentials or API key.');
+			results.text().then(text => console.error(text));
+		  }
 		let link = results.results[0].link
 		curSong = results.results[0].title;
+		
 
-		message.channel.send(`Now playing: ${htmlDecode(curSong)}`).then((result) => {
+		msgChannel.send(`Now playing: ${htmlDecode(curSong)}`).then((result) => {
 			console.log(result)
 			console.log("prevMsg", prevMsg)
 			console.log("result.id", result.id)
@@ -255,13 +299,16 @@ function searchYoutube(query) {
 		})
 
 		//client.prevMsg = ''
+		console.log("????")
 
 		return getYoutubeResource(link)
 	})
 	.then(resource => {
+		console.log("WHY")
 		playSong(resource)
+		console.log("WHY2")
 	})	
-	.catch( err => console.log(err));
+	.catch( err => console.log("heyyyy" + err));
 
 }
 
@@ -279,8 +326,11 @@ function playSong(resource) {
 	
 	// Play song
 	try{
+		console.log("?")
 		player.play(resource);
+		console.log("?!")
 		connection.subscribe(player);
+		console.log("?!!")
 	} catch (error) {
 		console.log('yup')
 		console.error(error);
@@ -323,6 +373,7 @@ function handleRequest(query) {
 
 		} else {
 			// Have to search for song
+			console.log("have to search youtube")
 			searchYoutube(query)
 		}
 
@@ -355,6 +406,7 @@ client.on(Events.MessageCreate, (message) => {
 		desc += '!unpause\n'
 		desc += '!skip\n'
 		desc += '!shuffle\n'
+		desc += '!qn <YouTube link / Spotify song link / Spotify playlist link / name of song>\n'
 		const helpEmbed = new EmbedBuilder()
 		.setColor(0x0099FF)
 		.setTitle('List of bot commands')
@@ -364,6 +416,7 @@ client.on(Events.MessageCreate, (message) => {
 			{ name: 'Pause audio', value: '**Command: **`!pause`' },
 			{ name: 'Unpause audio', value: '**Command: **`!unpause`' },
 			{ name: 'Shuffle queue', value: '**Command: **`!shuffle`' },
+			{ name: 'Queue next', value: '**Command: **`!qn <YouTube link / Spotify song link / Spotify playlist link / name of song>.`\n **Example**: !qn Pokemon theme song\n' },
 		)
 		return message.channel.send({ embeds: [helpEmbed] });
 	}
@@ -398,6 +451,20 @@ client.on(Events.MessageCreate, (message) => {
 		console.log("Trying to unpause...")
 		client.player.unpause()
 		return message.channel.send("Song unpaused")
+	}
+
+	else if (message.content.startsWith('!qn')) {
+		const query = message.content.slice(4);
+		if(query == '') { 
+			// Preview queue
+			embed = queueEmbed()
+			message.channel.send({ embeds: [embed] });
+
+		}else { 
+			// Add to queue
+			addToBegginingOfQueue(query)
+			message.reply(`Added to queue`);
+		}
 	}
 
 	else if (message.content.startsWith('!q')) {
@@ -461,6 +528,13 @@ client.on(Events.MessageCreate, (message) => {
 	//   }
 		
 	}
+	else if (message.content.startsWith('!playp')) {
+		const query = message.content.slice(7);
+		let test = 'https://open.spotify.com/playlist/5LFX7CQ59WmhXeHSSGIrXK?si=d0986224585f4378'
+		if (query == "test") {
+			handleRequest(test)
+		}
+	}
 	else {
 		console.log("not a message")
 		console.log(message.content)
@@ -492,4 +566,5 @@ client.on(Events.InteractionCreate, async interaction => {
 
 
 // Log in to Discord with your client's token
+//client.login(process.env.TOKEN);
 client.login(process.env.TOKEN);
